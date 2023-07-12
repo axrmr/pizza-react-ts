92824ddc4ef10019d95cb5b47ba71b3b85dc1ab3
olexandr.morar@gmail <olexandr.morar@gmail.com>
2023-07-12 19:10:06 +0300

    adding     vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  base: '/pizza-react-ts/',
+  plugins: [svgr(), react()],
+  resolve: {
+    alias: {
+      '~': '/src/',
+      API: '/src/API',
+      components: '/src/components',
+      scss: '/src/scss',
+      pages: '/src/pages',
+      consts: '/src/consts',
+      store: '/src/store',
+      hooks: '/src/hooks',
+    },
+  },
+  server: {
+    open: true,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+});
